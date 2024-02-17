004f6bc22a3daf323d206ae58b162c8411d8ac33
yusra-aina choudhury <yusrachoudhury123@gmail.com>
2024-02-17 23:42:30 +0000

    completed the first page styling

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Urbanist } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const UrbanistF = Urbanist({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Skynn",
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={UrbanistF.className}>{children}</body>
     </html>
   );
 }
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,12 +1,34 @@
 "use-client";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div style={{ backgroundColor: '#f3f6f1', minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <div>
-        <h1 className="text-xl text-center mt-10 text-gray-500">Homepage</h1>
-        <Image src="/logo.png" alt="Skynn Logo" width={800} height={200} />
+    <div className="min-h-screen flex flex-col items-center justify-center bg-[#F3F6F1] ">
+      <Image src="/logo.png" alt="Skynn Logo" width={300} height={300} />
+      <div className=" text-center space-y-4 mx-10 md:mx-[30vw] ">
+        <p className=" pt-10 text-lg  text-[#91968E]">
+          Find the latest skincare trends, what works for your skin &
+          transformative ingredients!
+        </p>
+        <p className=" text-lg text-[#91968E]">
+          Track your routine & keep up to date with your favourite
+          skinfluencers!
+        </p>
+      </div>
+      <div className=" flex flex-col gap-2 lg:flex-row lg:gap-5 pt-8">
+        <Link href="/sign-up">
+          <button className="bg-[#DCDFDA] border-[3px] border-[#A6A9A3] text-[#474547] w-56 py-1 rounded-full mt-5 text-lg ">
+            {" "}
+            Register{" "}
+          </button>
+        </Link>
+        <Link href="/sign-in">
+          <button className="bg-[#FEFFFD] border-[3px] border-[#A6A9A3] text-[#474547] w-56 py-1 rounded-full mt-5 text-lg ">
+            {" "}
+            Login{" "}
+          </button>
+        </Link>
       </div>
     </div>
   );
