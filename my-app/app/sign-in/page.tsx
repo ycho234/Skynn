f2e24294da58db46b7c5aee873ffa4be9c457346
yusra-aina choudhury <yusrachoudhury123@gmail.com>
2024-02-20 11:35:05 +0000

    first page styling

--- my-app/app/sign-in/page.tsx
+++ my-app/app/sign-in/page.tsx
@@ -15,7 +15,7 @@ const SignIn = () => {
     try {
       const res = await signInWithEmailAndPassword(email, password);
       console.log({ res });
-      sessionStorage.setItem("user", String(true)); // Convert boolean to string
+      sessionStorage.setItem("user", String(true));
       setEmail("");
       setPassword("");
       router.push("/");
@@ -25,33 +25,22 @@ const SignIn = () => {
   };
 
   return (
-    <div className="min-h-screen flex-col cols-1 items-center justify-center bg-customLightGreen">
-      <div><Image src="/favicon.png" alt="Skynn Logo" width={80} height={20} />
-      </div>
-      <div className="bg-customLightGreen p-10 rounded-lg shadow-xl w-96">
-        <h1 className="text-customDarkGreen text-2xl mb-5">Sign In</h1>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="w-full p-3 mb-4 bg-gray-500 rounded-3xl outline-none text-white placeholder-gray-500 border-solid border-2 border-gray-400"
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="w-full p-3 mb-4 bg-gray-500 rounded-3xl outline-none text-white placeholder-gray-500 border-solid border-2 border-gray-400"
-        />
-        <button
-          onClick={handleSignIn}
-          className="w-full p-3 bg-customDarkGreen rounded-3xl text-white hover:bg-lime-800"
-        >
-          Sign In
-        </button>
-      </div>
-    </div>
+    <>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={handleSignIn}>Sign In</button>
+    </>
   );
 };
 
