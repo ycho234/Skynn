53727a8982b0c89427317626f62bcdfbf78bab0e
“Katie-W-22” <katiecoder22@gmail.com>
2024-02-29 15:03:58 +0000

    search input on products page with logo

--- my-app/app/components/viewAllProducts.tsx
+++ my-app/app/components/viewAllProducts.tsx
@@ -65,13 +65,22 @@ export default function ViewAllProducts() {
     fetchProducts();
   }, []);
 
-  return (
-    <div className="bg-customLightGreen grid grid-cols-2 gap-1">
+  return (<>
+  <div className="bg-customLightGreen grid grid-col-1 justify-center items-center">
+  <Image src="/logo.png" alt="Skynn Logo" width={160} height={40} className="pt-6 w-80"/>
+
+  <input type="text" placeholder="Search for products" className="border-2 border-gray-300 rounded-2xl p-1 my-4 w-80"/>
+</div>
+
+  <h1 className="text-center text-xl font-bold py-4">Trending Products Go Here</h1>
+  <h2 className="text-2xl py-2 bg-customLightGreen">View All Products</h2>
+    <div className="bg-customLightGreen grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1">
+      
       {productsList &&
         productsList.map((product) => (
           <div key={product.id}>
             <div
-              className="border-gray-300 border-4 mx-10 my-10 flex justify-center bg-white rounded-lg"
+              className="border-gray-300 border-2 mx-10 my-10 flex justify-center bg-white rounded-xl shadow-lg"
             >
               <Image
                 src={product.photo}
@@ -81,11 +90,12 @@ export default function ViewAllProducts() {
                 className="self-center"
               />
             </div>
-            <p className="text-center"> {product.brand}</p>
+            <p className="text-center"> {product.brand.toUpperCase()}</p>
             <p className="text-center">{product.name}</p>
-            <p className="text-center">Rating {getStarRating(product.rating)}</p>
+            <p className="text-center">Rating {getStarRating(product.rating)}{product.rating}</p>
           </div>
         ))}
     </div>
+    </>
   ); 
 }
