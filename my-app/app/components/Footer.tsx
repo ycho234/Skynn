ed5610354661d70b1ffbada3d824d914135e3127
“Katie-W-22” <katiecoder22@gmail.com>
2024-03-01 11:11:55 +0000

    lucide icons added to footer

--- my-app/app/components/Footer.tsx
+++ my-app/app/components/Footer.tsx
@@ -1,29 +1,34 @@
 import Link from "next/link"
+import {Search, TrendingUp, Smile, User} from 'lucide-react'
 
 export default function Footer() {
     return (
     <>
-    <div className="flex items-center justify-evenly py-2 border-t fixed border-gray-200 border-solid bottom-0 w-full bg-orange-100">
+    <div className="flex items-center justify-evenly py-2 border-t fixed border-gray-200 border-solid bottom-0 w-full bg-[#d0d6ca]">
   <div className="flex space-x-14">
     <div className="flex flex-col items-center">
       <Link href="/" className="flex items-center">
-      <span>Home</span>
+      <Search size={24} />
       </Link>
+      <span>Home</span>
     </div>
     <div className="flex flex-col items-center">
       <Link href="/products" className="flex items-center">
-      <span>Products</span>
+      <TrendingUp size={24} />
       </Link>
+      <span>Products</span>
     </div>
     <div className="flex flex-col items-center">
       <Link href="/sign-in" className="flex items-center">
-      <span>Sign In</span>
+      <Smile size={24} />
       </Link>
+      <span>Sign In</span>
     </div>
     <div className="flex flex-col items-center">
       <Link href="/sign-up" className="flex items-center">
-      <span>Sign Up</span>
+      <User size={24} />
       </Link>
+      <span>Sign Up</span>
     </div>
   </div>
 </div>
