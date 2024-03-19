7972bf8a8ea0da8d3393c10a142230d597bbfe15
yusra-aina choudhury <yusrachoudhury123@gmail.com>
2024-03-19 13:15:17 +0000

    installing mui

--- my-app/app/sign-out/page.tsx
+++ my-app/app/sign-out/page.tsx
@@ -1,7 +1,8 @@
-'use client';
+"use client";
 import { getAuth, signOut } from "firebase/auth";
-import {app} from "@/app/firebase/config";
+import { app } from "@/app/firebase/config";
 import { useRouter } from "next/navigation";
+import ResponsiveDrawer from "../components/menu";
 
 const auth = getAuth(app);
 
@@ -22,6 +23,7 @@ const SignOut = () => {
   return (
     <div>
       <button onClick={handleSignOut}>Sign Out</button>
+      <ResponsiveDrawer />
     </div>
   );
 };
